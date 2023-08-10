a019e9136e1f3644f0ee2ad6d6cdf581106c2275
SCM <hys1693359@gmail.com>
2023-08-10 09:48:46 +0900

    commit test

--- newproject/src/sample/TernaryEx.java
+++ newproject/src/sample/TernaryEx.java
@@ -4,6 +4,6 @@ public class TernaryEx {
   public static void main(String[] args) {
     int x = 2;
    int y = (x == 1) ? 10 : 20;
-    System.out.println("y = " + y);
+    System.out.println("y = " + y); //수정 테스트
   }
 }
